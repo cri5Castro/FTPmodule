27cb3b24a9d9b84a8f86fcfe8adf28a88962af3e
cri5castro <djcriz5@gmail.com>
2019-05-29 13:13:42 -0500

    configure and getconfiguration implemented

--- configurator/app.py
+++ configurator/app.py
@@ -12,6 +12,7 @@ y vicevers usando telnet generar startup congif usando copy y descargarlo
 
 
 """
+GENERATE_CONFIG=["enable","copy run start"]
 
 def interactTelnet(options):
     client=TelnetClient(options.addr,options.user,options.password)
@@ -36,12 +37,19 @@ def download(options):
     client.getFile(options.ipath,options.opath)
     
 def configure(options):
-    pass
+    print(f'Configuring router at {options.addr}')
+    upload(options)
+    print("restart router to apply configuration")
 
-def getConfiguration(options):
-    pass
+
+def getConfiguration(options):#TODO
+    print(f'get configuration of router at {options.addr}')
+    clientTn = TelnetClient(options.addr,options.user,options.password)
+    clientTn.sendCommands(GENERATE_CONFIG)
+    download(options)
     
 def getInventory(options):
+    print(f'Generating Inventory for addr: {options.addr} community: {options.com}')
     client  = SNMPClient(options.addr,options.com)
     client.getInventory()
 
@@ -89,6 +97,24 @@ def run(args):
     parser_getInventory.add_argument('addr',help="ip address to connect with")
     parser_getInventory.add_argument('com',help="snmp community of the agent",default="rcp")
     parser_getInventory.set_defaults(func=getInventory)
+
+    # Create a configure subcommand    
+    parser_configure = subparsers.add_parser('configure',aliases=["config","c"], help='upload a startup config file to a FTP SERVER with address <addr>')
+    parser_configure.add_argument('addr',help="ip address to connect with")
+    parser_configure.add_argument('-ipath', '-i', help="Path of the file to be sent",default='startup-config')
+    parser_configure.add_argument('-opath', '-o', help="Path to store the file",default='startup-config')
+    parser_configure.add_argument('-user','-u',help="FTP user",default="rcp")
+    parser_configure.add_argument('-password','-p',help="FTP password",default="rcp")
+    parser_configure.set_defaults(func=configure)
+    
+    #create getConfiguration subcommand
+    parser_getConfiguration = subparsers.add_parser('getConfiguration',aliases=["gc","getconfig"],help='download  the startup-config file from a FTP SERVER with address <addr>')
+    parser_getConfiguration.add_argument('addr',help="ip address to connect with")
+    parser_getConfiguration.add_argument('-ipath', '-i', help="Path to store the file",default='startup-config')
+    parser_getConfiguration.add_argument('-opath', '-o', help="Path to the desired file",default='startup-config')
+    parser_getConfiguration.add_argument('-user','-u',help="FTP user",default="rcp")
+    parser_getConfiguration.add_argument('-password','-p',help="FTP password",default="rcp")
+    parser_getConfiguration.set_defaults(func=getConfiguration)
     
     
     
