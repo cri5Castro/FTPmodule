9f089c6d31138ab49743b89e019f2a8ad936f2ed
Juan Carlos Garcia Medina <carlos_isc7@outlook.com>
2019-05-29 13:22:27 -0500

    changes saved

--- configurator/SNMP/SNMPClient.py
+++ configurator/SNMP/SNMPClient.py
@@ -34,7 +34,8 @@ class SNMPClient:
 	    elif errorStatus:
 	        print('%s at %s' % (errorStatus.prettyPrint(),errorIndex and varBinds[int(errorIndex) - 1][0] or '?'))
 	    else:
-	        for varBind in varBinds:
+	    	for varBind in varBinds:
 	            varB=(' = '.join([x.prettyPrint() for x in varBind]))
+	            #print(varBind)
 	            resultado= varB.split()[2]
 	    return resultado
